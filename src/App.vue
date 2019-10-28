8ecbe63478cdc58ba2fcd236bb30efbe65773a0b
Yol-24 <54098157+Yol-24@users.noreply.github.com>
2019-10-28 10:01:35 +0800

    modified

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
   <v-app>
     <v-content>
        <div id="app">
-        <div id="nav">
-            <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
-        </div>
-        <router-view @authenticated="setAuthenticated" />
+        <router-view  />
     </div>
     </v-content>
   </v-app>
@@ -15,24 +12,7 @@
  export default {
         name: 'App',
         data() {
-            return {
-                authenticated: false,
-                    username: "yol",
-                    password: "Torres"
-            }
         },
-        mounted() {
-            if(!this.authenticated) {
-                this.$router.replace({ name: "login" });
-            }
-        },
-        methods: {
-            setAuthenticated(status) {
-                this.authenticated = status;
-            },
-            logout() {
-                this.authenticated = false;
-            }
-        }
+ 
     }
 </script>
